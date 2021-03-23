f9f5750ee70eda695fe7cc741f35ed7cbffcfcfa
regina <brf001@campus.mephi.ru>
2021-03-23 19:07:04 +0300

    designed sign-in (why is it so fcking hard? and it doesn't even look super-pretty? kill me)

--- front/src/components/Login/index.tsx
+++ front/src/components/Login/index.tsx
@@ -1,13 +1,40 @@
 import * as React from 'react';
-import { Button, InputGroup } from '@blueprintjs/core';
 import s from './LoginForm.module.scss'
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeLogin, changePassword, loginUser } from './@slice';
-import { Redirect } from 'react-router-dom';
-import Routes from '../../pages/routes';
 import {useEffect} from "react";
 import {clearState} from './@slice'
 import {useHistory} from 'react-router-dom'
+
+import TextField from '@material-ui/core/TextField'
+
+import Container from '@material-ui/core/Container'
+import {Avatar, Box, CssBaseline, makeStyles, Typography} from "@material-ui/core";
+import {Button, Icon} from '@blueprintjs/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        width: '100%',
+    }
+}));
+
+
 
 const LoginForm: React.FC  = () => {
   const login = useAppSelector(state => state.loginForm.login);
@@ -16,6 +43,7 @@ const LoginForm: React.FC  = () => {
   let isAuth = useAppSelector(state => state.loginForm.isAuth);
   const dispatch = useAppDispatch();
   const history = useHistory();
+  const classes = useStyles();
 
   useEffect(()=> {
       if(isAuth) {
@@ -25,19 +53,47 @@ const LoginForm: React.FC  = () => {
   }, [isAuth]);
 
   return (
-    <div className={s.root}>
-      <h3>{status}</h3>
-      <InputGroup id="username" placeholder="Enter your login..."
-                  value={login}
-                  onChange={(event) => dispatch(changeLogin(event.target.value))}/>
-      <InputGroup id="password" placeholder="...and password"
-                  value={password}
-                  onChange={(event) => dispatch(changePassword(event.target.value))}/>
-      <Button icon="send-to" intent="success" text="Войти" onClick={
-        () => dispatch(loginUser({login, password}))
-      }/>
-    </div>
+      <div className={s.root}>
+          <div className={classes.paper}>
+
+                  <Icon icon={'heart-broken'} intent={'danger'} iconSize={60}/>
+
+              <Box fontFamily={"Monospace"} fontSize={'h2.fontSize'} m={1}>
+                  Sign in
+              </Box>
+              <form className={classes.form}>
+                  <TextField
+                      variant={'outlined'}
+                      margin={'normal'}
+                      required
+                      fullWidth
+                      id={'username'}
+                      label={"Your login"}
+                      name={'login'}
+
+                      value={login}
+                      onChange={(event) => dispatch(changeLogin(event.target.value))}
+                  />
+                  <TextField
+                      variant={'outlined'}
+                      margin={'normal'}
+                      required
+                      fullWidth
+                      name={'password'}
+                      label={"Your password"}
+                      type={'password'}
+                      id={'password'}
+                      value={password}
+                      onChange={(event) => dispatch(changePassword(event.target.value))}
+                  />
+                  <Button icon="send-to" intent="none" className={classes.submit} text="Войти" onClick={
+                      () => dispatch(loginUser({login, password}))
+                  }/>
+              </form>
+          </div>
+      </div>
   )
 }
+
 
 export default LoginForm;
