25d433baff662ef824e28559f817014e52db1095
regina <brf001@campus.mephi.ru>
2021-04-09 00:29:28 +0300

    koroche experimenti s gitom to be continued ahaha

--- front/src/pages/News/index.tsx
+++ front/src/pages/News/index.tsx
@@ -1,13 +1,12 @@
+// @ts-ignore
 import React, { useEffect } from 'react';
 import NewsList from '../../components/NewsList';
 import NavBar from '../../components/NavBar';
 
 const News: React.FC = () => {
-
   useEffect(() => {
     //getNewsList();
   },[]);
-
   return(
     <div>
       <NavBar/>
