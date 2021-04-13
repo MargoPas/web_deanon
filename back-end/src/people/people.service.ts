a3f19514de1e23be3c21a4d1a75923b6c65ec6eb
MargoPas <yamargarita2001@mail.ru>
2021-04-13 22:17:46 +0300

    Create people

--- back-end/src/people/people.service.ts
+++ back-end/src/people/people.service.ts
@@ -11,16 +11,15 @@ export class PeopleService {
     private readonly PeopleRepository: Repository<People>,
   ) {}
 
-  async create(CreateUserDto: CreatePeopleDto) {
+  async create(CreateUserDto: CreatePeopleDto, photoUrl: string) {
     const people = this.PeopleRepository.create(CreateUserDto);
-    console.log(people);
+    people.Photo = photoUrl;
     await this.PeopleRepository.save(people);
   }
+
   async setPhoto(userId: number, photoUrl: string) {
     try {
-      console.log(userId);
       const user = await this.PeopleRepository.findOne({ id: userId });
-      console.log(user, 'user');
       await this.PeopleRepository.update({ id: userId }, { Photo: photoUrl });
     } catch (e) {
       return { message: "Can't upload your photo" };
