5056d4475d0160918c8683f51e5eedd2805a40f4
maulitt <70881278+maulitt@users.noreply.github.com>
2021-04-20 22:30:09 +0300

    temporary changes on my way to making search

---
+++ front/src/pages/Results/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import NavBar from "../../components/NavBar";
+import Results from '../../components/Results';
+
+export const ResultsPage: React.FC = (props) => {
+    return (
+        <div>
+            <NavBar/>
+
+        </div>
+    );
+}
